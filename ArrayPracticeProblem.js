da20aff55fdec348103158f5256a9b8cf0dc5408
tutorial6 <manapure.vaibhav542@gmail.com>
2022-08-03 18:06:52 +0530

    Que3- Calculating Prme Factors

--- ArrayPracticeProblem.js
+++ ArrayPracticeProblem.js
@@ -1,50 +1,68 @@
-//Que1:Storing 3 Digit Number in Array & finding smallest & largest.RG----
+// //Que1:Storing 3 Digit Number in Array & finding smallest & largest.RG----
 
-function GetThreeDigitRandomNumber() {
-    return Math.floor(Math.random() * 900 + 100);
+// function GetThreeDigitRandomNumber() {
+//     return Math.floor(Math.random() * 900 + 100);
+//   }
+// //Array of 3 Digit Number
+// let arrayOf3DigitRandomNumber = new Array();
+// //Storing array elements
+// for (let i = 0; i < 10; i++){
+//     arrayOf3DigitRandomNumber.push(GetThreeDigitRandomNumber());
+// }
+// console.log("Ten 3 digits Random numbers are: " + arrayOf3DigitRandomNumber.toString());
+
+// FindLargestAndSmallest(arrayOf3DigitRandomNumber);
+// //Functiion for getting first & second largest smallest
+// function FindLargestAndSmallest(arrayOfElements){
+//     let firstLargest = 0,
+//         secondlargest = 0;
+//     let firstSmallest = 1000,
+//         secondSmallest = 1000;
+//     for (let i = 0; i < arrayOfElements.length; i++) {
+//         if (arrayOfElements[i] > firstLargest) {
+//         secondlargest = firstLargest;
+//         firstLargest = arrayOfElements[i];
+//         } else if (
+//         arrayOfElements[i] > secondlargest &&
+//         arrayOfElements[i] != firstLargest
+//         )
+//         secondlargest = arrayOfElements[i];
+//     }
+//     for (let i = 0; i < arrayOfElements.length; i++) {
+//         if (arrayOfElements[i] < firstSmallest) {
+//         secondSmallest = firstSmallest;
+//         firstSmallest = arrayOfElements[i];
+//         } else if (
+//         arrayOfElements[i] < secondSmallest &&
+//         arrayOfElements[i] != firstSmallest
+//         )
+//         secondSmallest = arrayOfElements[i];
+//     }
+// console.log("First largest number is: " + firstLargest);
+// console.log("First smallest number is: " + firstSmallest);
+// console.log("Second smallest number is: " + secondSmallest);
+// console.log("Second largest number is: " + secondlargest);
+// };
+
+// console.log("\nUsing Array Sort Method");
+// arrayOf3DigitRandomNumber.sort();
+// console.log("Second Largest Number is : "+arrayOf3DigitRandomNumber[1]);
+// console.log("Second Smallest Number is : "+arrayOf3DigitRandomNumber[8]);
+/*-------------------------------------------------*/
+//Que3-Calculating Prime Factors
+function PrimeFactors(number) {
+    let array = new Array();
+    for (let i = 2; i <= number / 2; i++) {
+      if (number % i == 0) {
+        let count = 0;
+        for (let j = 2; j <= Math.sqrt(i); j++) {
+          if (i % j == 0) count++;
+        }
+        if (count == 0 && i > 1) array.push(i);
+      }
+    }
+    return array;
   }
-//Array of 3 Digit Number
-let arrayOf3DigitRandomNumber = new Array();
-//Storing array elements
-for (let i = 0; i < 10; i++){
-    arrayOf3DigitRandomNumber.push(GetThreeDigitRandomNumber());
-}
-console.log("Ten 3 digits Random numbers are: " + arrayOf3DigitRandomNumber.toString());
-
-FindLargestAndSmallest(arrayOf3DigitRandomNumber);
-//Functiion for getting first & second largest smallest
-function FindLargestAndSmallest(arrayOfElements){
-    let firstLargest = 0,
-        secondlargest = 0;
-    let firstSmallest = 1000,
-        secondSmallest = 1000;
-    for (let i = 0; i < arrayOfElements.length; i++) {
-        if (arrayOfElements[i] > firstLargest) {
-        secondlargest = firstLargest;
-        firstLargest = arrayOfElements[i];
-        } else if (
-        arrayOfElements[i] > secondlargest &&
-        arrayOfElements[i] != firstLargest
-        )
-        secondlargest = arrayOfElements[i];
-    }
-    for (let i = 0; i < arrayOfElements.length; i++) {
-        if (arrayOfElements[i] < firstSmallest) {
-        secondSmallest = firstSmallest;
-        firstSmallest = arrayOfElements[i];
-        } else if (
-        arrayOfElements[i] < secondSmallest &&
-        arrayOfElements[i] != firstSmallest
-        )
-        secondSmallest = arrayOfElements[i];
-    }
-console.log("First largest number is: " + firstLargest);
-console.log("First smallest number is: " + firstSmallest);
-console.log("Second smallest number is: " + secondSmallest);
-console.log("Second largest number is: " + secondlargest);
-};
-
-console.log("\nUsing Array Sort Method");
-arrayOf3DigitRandomNumber.sort();
-console.log("Second Largest Number is : "+arrayOf3DigitRandomNumber[1]);
-console.log("Second Smallest Number is : "+arrayOf3DigitRandomNumber[8]);
+  let number = Math.floor(Math.random() * 1000);
+  console.log("Prime Factors of " + number + " are : ");
+  console.log(PrimeFactors(number));
